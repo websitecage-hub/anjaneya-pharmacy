695dc2f39b5862e7a5695fc8e0cf5cb26e6599d5
websitecage-hub <websitecage@gmail.com>
2026-02-21 20:36:03 +0530

    Fix: Update repository URL for new account and rebuild project

--- deploy.bat
+++ deploy.bat
@@ -3,5 +3,5 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin https://github.com/solefreshofficial/anjaneya-pharmacy-glow.git
+git remote add origin https://github.com/websitecage-hub/anjaneya-pharmacy.git
 git push -u origin main
