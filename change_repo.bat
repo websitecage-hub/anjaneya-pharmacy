8b83a809a52bcb05b245b8926c9a728c0d6a4d63
solefreshofficial <solefreshofficial@gmail.com>
2026-02-21 18:54:39 +0530

    Update base path for anjaneya-pharmacy

---
+++ change_repo.bat
@@ -0,0 +1,5 @@
+@echo off
+git remote set-url origin https://github.com/solefreshofficial/anjaneya-pharmacy.git
+git add .
+git commit -m "Update base path for anjaneya-pharmacy"
+git push
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/anjaneya-pharmacy-glow/',
+  base: '/anjaneya-pharmacy/',
   server: {
     host: "::",
     port: 8080,
